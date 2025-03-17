a5d14b0c87c56dcd7fd96b6960f6ad69a1b06066
Dennzae <146009438+Dennzae@users.noreply.github.com>
2025-03-17 14:17:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ def main():
   st.title('Dermatology Machine Learning')
   st.info('This Using Machine learning')
 
-  erythema = st.radio('Erythema',min_value=0,max_value=3,value=2)
+  erythema = st.slider('Erythema',min_value=0,max_value=3,value=2)
 
 if __name__ == "__main__":
   main()
